aef0efa5ec5f0b821e6b2be4bb8e05eadd2e4339
Owen Cruse <ocruse7@gmail.com>
2020-10-29 12:18:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,7 @@
-def display_board()
-  puts "   |   |    \n-----------\n   |   |    \n-----------\n   |   |    "
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
